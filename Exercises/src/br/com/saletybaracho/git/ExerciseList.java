a28e7c4fcefc3a86237ab1be4e4ae496d314960c
Salety Ferreira Baracho <salety.baracho@autaza.com>
2020-10-11 11:42:57 -0300

    Adicionei uma linha no código.

--- Exercises/src/br/com/saletybaracho/git/ExerciseList.java
+++ Exercises/src/br/com/saletybaracho/git/ExerciseList.java
@@ -15,6 +15,7 @@ public class ExerciseList {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello!");
+        System.out.print("Salety Baracho.");
 	}
 
 }
