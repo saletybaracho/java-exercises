c5ba44da1e9bf4fa51f36b7c1a9f3bcfd070a24a
saletybaracho <saletybaracho@users.noreply.github.com>
2020-10-11 11:35:55 -0300

    Adicionando arquivos via upload.

---
+++ Exercises/src/br/com/saletybaracho/git/ExerciseList.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.saletybaracho.git;
+
+/**
+ * @author Salety
+ *
+ */
+public class ExerciseList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello!");
+	}
+
+}
